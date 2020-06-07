46bee74ce5121d95b4248df864888640df8a431e
Angela Cullen <angela@paddle.com>
2020-06-07 21:57:17 +0100

    added a checkbox index file

---
+++ src/components/atoms/checkbox/index.tsx
@@ -0,0 +1,1 @@
+// There isn't really a checkbox here. I added this as part of DEV-103-testing from inside the fake-library repo. You should be able to pull this branch down to the fake-front-end repo to test.
